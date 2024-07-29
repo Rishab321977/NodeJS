c1e931cafb09ac2f2386756e4e4fd350398bbceb
Rishab U Jain <rishab321977@gmail.com>
2024-07-29 13:14:39 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Mongo DB connection failed!!", err);
+  });
